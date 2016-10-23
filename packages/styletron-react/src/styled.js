90701b86a07cf89e3e36a5db2927f59eba277e71
Ryan Tsao <rtsao@uber.com>
2016-10-22 22:44:26 -0700

    Updated jsdocs

--- packages/styletron-react/src/styled.js
+++ packages/styletron-react/src/styled.js
@@ -8,6 +8,41 @@ const TAG_KEY = '__STYLETRON_TAG';
 
 module.exports = styled;
 
+/**
+ * Helper function to create styled element components
+ * @packagename styletron-react
+ * @param  {String|function} base     Tag name or styled element component
+ * @param  {function|object} styleFn  Style object or function that returns a style object
+ * @return {function}                 Styled element component
+ * @example
+ * import {styled} from 'styletron-react';
+ *
+ * const Panel = styled('div', {
+ *   backgroundColor: 'lightblue',
+ *   fontSize: '12px'
+ * });
+ *
+ * <Panel>Hello World</Panel>
+ * @example
+ * import {styled} from 'styletron-react';
+ *
+ * const Panel = styled('div', (props) => ({
+ *   backgroundColor: props.alert ? 'orange' : 'lightblue',
+ *   fontSize: '12px'
+ * }));
+ *
+ * <Panel alert>Danger!</Panel>
+ * @example
+ * import {styled} from 'styletron-react';
+ *
+ * const DeluxePanel = styled(Panel, (props) => ({
+ *   backgroundColor: props.alert ? 'firebrick' : 'rebeccapurple',
+ *   color: 'white',
+ *   boxShadow: '3px 3px 3px darkgray'
+ * }));
+ *
+ * <DeluxePanel>Bonjour Monde</DeluxePanel>
+ */
 function styled(base, styleArg) {
   if (typeof base === 'string') {
     // Element
--- packages/styletron-server/src/styletron-server.js
+++ packages/styletron-server/src/styletron-server.js
@@ -20,16 +20,39 @@ class StyletronServer extends StyletronCore {
     return super.injectDeclaration(decl);
   }
 
+  /**
+   * Get an array of stylesheet objects
+   * @return {Array} Array of stylesheet objects
+   * @example
+   * const styletron = new StyletronServer();
+   *
+   * styletron.injectDeclaration({prop: 'color', val: 'red'});
+   * // → 'c0'
+   * styletron.getStylesheets();
+   * // → [{css: '.c0{color:red}'}]
+   */
   getStylesheets() {
     return cacheToStylesheets(this.cache);
   }
 
+  /**
+   * Get a string of style elements for server rendering
+   * @return {String} The string of HTML
+   * @example
+   * const styletron = new StyletronServer();
+   *
+   * styletron.injectDeclaration({prop: 'color', val: 'red'});
+   * // → 'c0'
+   * styletron.getStylesheetsHtml();
+   * // → '<style class="styletron">.c0{color:red}</style>'
+   */
   getStylesheetsHtml(className = 'styletron') {
     return generateHtmlString(this.getStylesheets(), className);
   }
 
   /**
-   * Get the CSS string of cache
+   * Get the CSS string. For hydrating styles on the client,
+   * [`getStylesheetsHtml`]{@link StyletronServer#getStylesheetsHtml} or [`getStylesheets`]{@link StyletronServer#getStylesheets} should be used instead. 
    * @return {String} The string of CSS
    * @example
    * const styletron = new StyletronServer();
