3d599578c5b27a8640309d54677e5523e397f491
Ryan Tsao <rtsao@uber.com>
2016-12-23 16:33:56 -0800

    Added css property caching

--- packages/styletron-utils/src/hyphenate-style-name.js
+++ packages/styletron-utils/src/hyphenate-style-name.js
@@ -1,11 +1,14 @@
 const uppercasePattern = /[A-Z]/g;
 const msPattern = /^ms-/;
+const cache = {};
 
 module.exports = hyphenateStyleName;
 
 function hyphenateStyleName(prop) {
-  return prop
-    .replace(uppercasePattern, '-$&')
-    .toLowerCase()
-    .replace(msPattern, '-ms-');
+  return prop in cache
+    ? cache[prop]
+    : cache[prop] = prop
+      .replace(uppercasePattern, '-$&')
+      .toLowerCase()
+      .replace(msPattern, '-ms-');
 }
