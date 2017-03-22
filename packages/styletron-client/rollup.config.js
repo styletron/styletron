2948b149e43ad27f39f14d2527b2e4d0ca20610a
Ludovico Fischer <livrerie@gmail.com>
2017-03-22 19:33:24 +0100

    Add UMD bundles for styletron client.
    Fixes #77.

--- packages/styletron-client/rollup.config.js
+++ packages/styletron-client/rollup.config.js
@@ -6,7 +6,8 @@ export default {
   entry: 'src/styletron-client.js',
   targets: [
     { dest: 'dist/bundle.cjs.js', format: 'cjs'},
-    { dest: 'dist/bundle.es.js', format: 'es'}
+    { dest: 'dist/bundle.es.js', format: 'es'},
+    { dest: 'dist/bundle.umd.js', format: 'umd', moduleName: 'StyletronClient'}
   ],
   plugins: [
     nodeResolve(),
