9255b8bc4ca553066aa31d856ab32f66af45c6fa
Ludovico Fischer <livrerie@gmail.com>
2017-03-22 17:05:19 +0100

    Build styletron-client with rollup.

---
+++ packages/styletron-client/rollup.config.js
@@ -0,0 +1,17 @@
+import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import buble from 'rollup-plugin-buble';
+
+export default {
+  entry: 'src/styletron-client.js',
+  format: 'cjs',
+  targets: [
+    { dest: 'dist/bundle.cjs.js', format: 'cjs'},
+    { dest: 'dist/bundle.es.js', format: 'es'}
+  ],
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    buble()
+  ]
+};
