aa685e72f081d63f73e4258a7fad6a3619067b12
Ryan Tsao <rtsao@uber.com>
2017-05-03 16:43:19 -0700

    Simplify styletron package

--- packages/styletron/src/index.js
+++
@@ -1,6 +0,0 @@
-import StyletronServer from 'styletron-server';
-import StyletronClient from 'styletron-client';
-
-const Styletron = __TARGET__ === 'browser' ? StyletronClient : StyletronServer;
-
-export default Styletron;
