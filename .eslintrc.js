92e0f6bf194b30f7fa2ae59172703932f1945809
Ryan Tsao <ryan.j.tsao@gmail.com>
2017-05-04 17:27:04 -0700

    Update lint configs

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+
+  parserOptions: {
+    ecmaVersion: 6
+  },
+
+  plugins: ['eslint-plugin-prettier'],
+
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        useTabs: false,
+        printWidth: 80,
+        tabWidth: 2,
+        singleQuote: true,
+        trailingComma: 'es5',
+        bracketSpacing: false,
+        jsxBracketSameLine: false,
+        parser: 'babylon',
+        semi: true,
+      },
+    ],
+  }
+
+};
