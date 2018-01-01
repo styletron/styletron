f9c11a5a09ff41f03267c851113267080642cb36
Ratson <github@ratson.name>
2018-01-01 20:30:35 +0800

    Avoid rendering empty class attribute

--- packages/styletron-server/src/generate-html-string.js
+++ packages/styletron-server/src/generate-html-string.js
@@ -4,8 +4,9 @@ function generateHtmlString(sheets, className) {
   let html = '';
   for (let i = 0; i < sheets.length; i++) {
     const sheet = sheets[i];
+    const classAttr = className ? ` class="${className}"` : '';
     const mediaAttr = sheet.media ? ` media="${sheet.media}"` : '';
-    html += `<style class="${className}"${mediaAttr}>${sheet.css}</style>`;
+    html += `<style${classAttr}${mediaAttr}>${sheet.css}</style>`;
   }
   return html;
 }
