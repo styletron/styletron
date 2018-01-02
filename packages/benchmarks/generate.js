f845083d6effe2519a206533cdc2cc1e069cc208
Ryan Tsao <ryan.j.tsao@gmail.com>
2018-01-01 21:12:00 -0800

    Delete in-repo benchmarks

--- packages/benchmarks/generate.js
+++
@@ -1,6 +0,0 @@
-require('./generate-synthetic-fixtures');
-require('./generate-styles');
-require('./generate-apps');
-require('./generate-bundles');
-require('./bundle');
-require('./generate-static');
