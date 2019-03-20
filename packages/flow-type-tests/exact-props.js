4134697308c0a3785a4a2e083cee454b012efdc6
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-03-19 18:10:44 -0700

    Add more exact cases

--- packages/flow-type-tests/exact-props.js
+++ packages/flow-type-tests/exact-props.js
@@ -10,3 +10,6 @@ const MyComponent: React.ComponentType<{|foo: 42|}> = _props => {
 const MyStyledComponent = styled(MyComponent, {color: "red"});
 
 <MyStyledComponent foo={42} />;
+
+// $FlowFixMe
+<MyStyledComponent foo={42} invalidProp={42} />; // Should fail, not in props
