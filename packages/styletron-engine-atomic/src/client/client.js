4e416dfff59739797fa24d126f2ee241200ac260
Vojtech Miksu <vojtech@miksu.cz>
2019-09-04 11:40:51 -0700

    Update packages/styletron-engine-atomic/src/client/client.js
    Co-Authored-By: Ryan Tsao <ryan.j.tsao@gmail.com>

--- packages/styletron-engine-atomic/src/client/client.js
+++ packages/styletron-engine-atomic/src/client/client.js
@@ -113,7 +113,7 @@ class StyletronClient implements StandardEngine {
       (media, _cache, insertBeforeMedia) => {
         const styleElement = document.createElement("style");
         styleElement.media = media;
-        if (typeof insertBeforeMedia === "undefined") {
+        if (insertBeforeMedia === void 0) {
           this.container.appendChild(styleElement);
         } else {
           const insertBeforeIndex = findSheetIndexWithMedia(
