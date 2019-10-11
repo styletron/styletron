014d1cf901325b5ad5d10cde92e9665a56843a78
Mo Kouli <mkouli@uber.com>
2019-10-11 15:59:01 -0700

    [new package][styletron-engine-snapshot]
    The styletron atomic-engine [auto-generates atomic css classes for you](https://www.styletron.org/#performance) making sure to minimize the number of classnames + style declarations when rendering.
    
    This is great for production as it ensures we only ever send the minimal amount of css necessary for render [and stop css size growth as we keep adding features](https://developers.facebook.com/videos/2019/building-the-new-facebookcom-with-react-graphql-and-relay/) (28:00), but it makes snapshot testing components brittle and adds noise to diffs.
    
    Snapshots generated with `styletron-engine-atomic` have 2 main issues
    
    1. non-debuggable classNames in snapshots ("ae af ")
    2. className generation is non-deterministic. If you create a new component,
    it might break an unrelated snapshot since that component rendering shifts the
    generation for the old snapshot.
    
    This was first brought up in #176 which ended in a recommendation to use the [no-op engine](https://github.com/styletron/styletron/issues/256)
    in testing so that snapshots do not contain any class/style information.
    
    Using the no-op engine means that snapshots lose information about classNames/styles, and only contain information about behavior and/or prop passing.
    This package attempts to provide a better solution for this problem.
    
    The package provides a *deterministic* engine that simply returns a `JSON.stringify`-ed version of the style object (with alpha-sorted keys).
    A snapshot should not fail **UNLESS** a style applied to the component changes (which you want).
    It also makes it easier to detect what exactly changed in the css in the snapshot since we actually now render all of the css values (we are essentially almost rendering the styles inline).
    
    Downsides of This Package/Approach
    
    1. The engine generates invalid css class names (as they are essentially JSON)
    2. Styles are **NOT ACTUALLY APPLIED**, so if you have existing tests that check/rely on computed styles, they no longer work.
    
    Future Improvements
    
    There is a path forward that can fix the downsides mentioned above.
    We can modify the engine into creating valid class names + style declerations **PER-STYLE**, similar to what https://acss.io/ does.
    That would make it so that snapshots still diff nicely, while actually applying styles as well.

--- packages/styletron-engine-snapshot/src/create-render.js
+++ packages/styletron-engine-snapshot/src/create-render.js
@@ -1,30 +1,16 @@
 // @flow
 
-import stringify from "json-stable-stringify";
-
 import {
   type StyleObject,
   type FontFaceObject,
   type KeyframesObject,
 } from "styletron-standard";
 
+import stringify from "json-stable-stringify";
+
 /*
-Problem, default atomic `Styletron` engine is really smart and meant for produciton.
-
-It creates atomic classes and only creates `classNames` as necessary.
-
-Unfortunately, for snapshot tests you end up with 2 issues:
-
-1) non-debuggable classNames in snapshots ("ae af ")
-2) className generation is non-deterministic. If you create a new component,
-it might break an unrelated snapshot since that component rendering shifts the
-generation for the old snapshot.
-
 This adds a "deterministic" engine that simply returns a `JSON.stringify`-ed
-version of the style object (with aplha-sorted keys). This makes it so that a
-snapshot should not fail **UNLESS** a style applied to the component changes (which you want).
-It also makes it easier to detect **what** exactly changed in the css in the snapshot since we
-actually now render all of the css values (we are essentially almost rendering the styles inline).
+version of the style object (with aplha-sorted keys).
 */
 export default function createRender(kind: string) {
   return function render(
