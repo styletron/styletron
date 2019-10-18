8f2977cba7923095977b5786c9a1f38222026e71
Gergely Nemeth <gnemeth@uber.com>
2019-10-18 15:48:03 -0700

    docs(contrib): add guide and mini app

---
+++ packages/playground/pages/_document.js
@@ -0,0 +1,39 @@
+import Document, {Head, Main, NextScript} from "next/document";
+import {Provider as StyletronProvider} from "styletron-react";
+import {styletron} from "../styletron";
+
+class MyDocument extends Document {
+  static getInitialProps({renderPage}) {
+    const page = renderPage(App => props => (
+      <StyletronProvider value={styletron}>
+        <App {...props} />
+      </StyletronProvider>
+    ));
+    const stylesheets = styletron.getStylesheets() || [];
+    return {...page, stylesheets};
+  }
+
+  render() {
+    return (
+      <html>
+        <Head>
+          {this.props.stylesheets.map((sheet, i) => (
+            <style
+              className="_styletron_hydrate_"
+              dangerouslySetInnerHTML={{__html: sheet.css}}
+              media={sheet.attrs.media}
+              data-hydrate={sheet.attrs["data-hydrate"]}
+              key={i}
+            />
+          ))}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    );
+  }
+}
+
+export default MyDocument;
