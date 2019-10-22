e25e6d1ac3c832a9f4448faf517c96b62b64e4d8
Ryan Tsao <ryan.j.tsao@gmail.com>
2019-10-22 14:58:40 -0700

    Add flow test for null and undefined values

---
+++ packages/flow-type-tests/null-and-undefined-values.js
@@ -0,0 +1,14 @@
+// @flow
+
+import * as React from "react";
+import {styled} from "styletron-react";
+
+const Foo = styled("div", {
+  // $FlowFixMe
+  zIndex: null, // null is not a valid value
+});
+
+const Bar = styled("div", {
+  // $FlowFixMe
+  zIndex: void 0, // undefined is not a valid value
+});
