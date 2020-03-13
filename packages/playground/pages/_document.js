116a217ac968c16d609ba027a4aff2c7d4506e0a
tajo <vojtech@miksu.cz>
2020-03-12 17:03:50 -0700

    revert playgournd changes

--- packages/playground/pages/_document.js
+++ packages/playground/pages/_document.js
@@ -1,4 +1,3 @@
-import * as React from "react";
 import Document, {Head, Main, NextScript} from "next/document";
 import {Provider as StyletronProvider} from "styletron-react";
 import {styletron} from "../styletron";
--- packages/playground/pages/index.js
+++ packages/playground/pages/index.js
@@ -1,5 +1,4 @@
-import {styled, useStyletron} from "styletron-react";
-import * as React from "react";
+import {styled} from "styletron-react";
 
 const Title = styled("h1", {
   color: "red",
@@ -12,16 +11,10 @@ const SubTitle = styled("h2", ({$size}) => ({
 }));
 
 export default () => {
-  const [show, setShow] = React.useState(false);
-  const [css] = useStyletron();
   return (
     <div>
       <Title>Title</Title>
       <SubTitle $size={50}>Subtitle</SubTitle>
-      <button onClick={() => setShow(!show)}>
-        {show ? "Hide" : "Show"} tiny title
-      </button>
-      {show && <h3 className={css({color: "hotpink"})}>Tiny title</h3>}
     </div>
   );
 };
