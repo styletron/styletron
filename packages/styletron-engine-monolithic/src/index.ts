a212f455d2d77daa8f297660cd2f8c1e37bb7db3
Vojtech Miksu <vojtech@uber.com>
2020-07-21 13:56:56 -0700

    fix build of styletron-engine-monolithic

--- packages/styletron-engine-monolithic/src/index.ts
+++ packages/styletron-engine-monolithic/src/index.ts
@@ -1,2 +1,5 @@
 export {default as Client} from "./client/client";
+export type {clientOptionsT, clientCacheT, hydrateT} from "./client/client";
+export type {StyleSheet, Options} from "./client/sheet";
 export {default as Server} from "./server/server";
+export type {serverOptionsT, serverCacheT, sheetT} from "./server/server";
